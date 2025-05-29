aa171592e9cb16d61e5b17f55f3ecc00993611ae
Labeeb-Neyazi <techlabeeb@gmail.com>
2025-05-29 22:45:04 +0530

    added file Index.html

---
+++ scriptt.js
@@ -0,0 +1,8 @@
+/*  =========  typing animation  =========  */
+var typed = new Typed(".typing",{
+    strings:["Web Developer","Frontend Developer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+function
